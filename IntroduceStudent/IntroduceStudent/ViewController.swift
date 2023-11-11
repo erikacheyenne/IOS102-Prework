a32553a8bf47a71076dbb5b0bd766138b09424aa
erikacheyenne <68251831+erikacheyenne@users.noreply.github.com>
2023-11-10 18:44:29 -1000

    Step 1 basic UI

---
+++ IntroduceStudent/IntroduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroduceStudent
+//
+//  Created by Erika Cheyenne on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
